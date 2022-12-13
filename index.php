652a912eb6069e7641ff264ed02d90561dc1f8e8
irvanda-febri-saputra <irvanda150202@gmail.com>
2022-12-13 20:02:28 +0700

    irvanweb

---
+++ index.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>SMKN 1 KOTA PROBOLINGGO</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: red;">
+    <div class="container-fluid">
+      <img src="https://smkn1probolinggo.sch.id/sekolah/wp-content/uploads/2020/12/cropped-cilik-e1607674409658.png" alt="logo">
+      <a class="navbar-brand" href="index.html">SMKN 1 KOTA PROBOLINGGO</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="index.php"><i class="fas fa-home"></i>
+            Beranda</a>
+          <a class="nav-link active" href=""><i class="fas fa-user"></i> Profile</a>
+          <a class="nav-link active" href=""><i class="fas fa-phone"></i> Kontak</a>
+        </div>
+
+      </div>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-danger" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  <!-- slide carousel -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="assets/img/slide1.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/slide2.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/slide3.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </a>
+  </div>
+  <!-- container menggunakan grid -->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm" style="background-color: white;">
+        <div class="media-body p-4 mt-2 text-center text-light">
+          <h4 class="heading" style="color: red;">Sambutan Kepala Sekolah</h4>
+          <p style="color: black">Assalamualaikum Warahmatullahi Wabarakaatuh.
+            Selamat Datang di website Sekolah SMKN 1 Probolinggo
+            Dengan Harapan bisa menjadikan media Informasi baik dilingkungan Sekolah maupun diluar Lingkungan Sekolah.
+
+            SMKN 1 Probolinggo Terus berupaya meningkatkan Kualitas pendidikan dan Pelayanan untuk menghasilkan siswa-siswa yang berkualitas baik secara Ilmu Pengetahuan maupun Ilmu Agama.
+
+            Website ini hadir sebagai salah satu media informasi dan pembelajaran Sekolah.
+
+            Semoga pendidikan di Negara kita bisa menjadi yang terbaik..</p>
+        </div>
+      </div>
+      <div class="col-sm" style="background-color: white;">
+        <div class="media-body p-4 mt-2 text-center text-light">
+          <h4 class="heading" style="color: red;">Visi dan Misi</h4>
+          <p style="color: black;">Visi <br>
+
+            Pada tahun 2024 menjadi sekolah unggul menciptakan SDM berwawasan IPTEK, berkarakter, berjiwa wirausaha, berprestasi, kompetitif dan siap kerja berstandar industri yang berazaskan IMTAQ <br>
+
+            Misi <br>
+
+            Misi Yang Telah Dirumuskan Untuk Pencapaian Visi Yang Akan Dilakukan Oleh SMKN 1 Probolinggo yaitu:
+
+            Menanamkan karakter dan etika yang berasaskan iman dan taqwa kepada Tuhan YME
+            Mengembangkan budaya kerja industri, dunia usaha dan dunia kerja
+            Mengembangkan IPTEK dalam rangka meningkatkan kualitas SDM yang unggul
+            Meningkatkan jiwa wirausaha yang mampu bersaing di era digital
+            Meningkatkan kompetensi SDM berkualitas yang mampu bersaing di tingkan nasional dan internasional</p>
+        </div>
+      </div>
+      <div class="col-sm" style="background-color: white;">
+        <div class="media-body p-4 mt-2 text-center text-light">
+
+          <h4 class="heading" style="color: red;">Sejarah</h4>
+          <p style="color: black;">SMKN 1 Probolinggo semula berstatus SMEA swasta yang berdiri dan beroperasi sejak 1 Agustus 1960. Pemrakarsa berdirinya SMEA adalah guru-guru SMEP Negeri Probolinggo. Hanya ada satu jurusan, yaitu Koperasi. Tempat penyelenggaraan proses belajar mengajar masih menumpang pada SMP Negeri yang beralamat di Jl. Dr. Moh. Saleh No. 7 Probolinggo, dilaksanakan pada sore hari.
+          </p>
+        </div>
+      </div>
+
+
+
+    </div>
+    <!-- container menggunakan grid -->
+    <div class="row">
+      <div class="col" style="text-align: center;">
+        <h1>Kontak Kami</h1>
+        <p>
+          Silahkan tinggalkan pesan anda, pada kolom yang tersedia.
+        </p>
+
+      </div>
+
+      <div class="col">
+        <form method="post" action="#">
+
+          <div class="form-group">
+            <label for="">Nama Anda:</label>
+            <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama">
+          </div>
+
+          <div class="form-group">
+            <label for="">E-mail Anda:</label>
+            <input type="email" class="form-control" name="email" placeholder="Masukkan Email">
+          </div>
+
+          <div class="form-group">
+            <label for="">Pesan Anda:</label>
+            <textarea name="pesan" class="form-control" cols="30" rows="7"></textarea>
+          </div>
+        </form><br>
+        <input class="btn btn-primary" type="submit" value="KIRIM">
+      </div>
+    </div>
+    <br>
+
+    <!-- grid footer -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm" style="background-color: red;">
+          <div class="media-body p-4 mt-2 text-center text-light">
+            <p>SMK 1 KOTA PROBOLINGGO</p>
+
+          </div>
+          <div class="container py-4" style="text-align: cent                                                                                                                        er;">
+            <div class="copyright" style="color: white;">
+              &copy; Copyright <strong><span>SMKN 1 KOTA PROBOLINGGO</span></strong>. All Rights Reserved
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
